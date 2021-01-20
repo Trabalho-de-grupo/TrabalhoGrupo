0d1c81023e16ccf051365daf57408ba09972c814
Beatrizlopes98 <40200198@esmad.ipp.pt>
2021-01-20 21:50:23 +0000

    ihaekbj

---
+++ scripts/scripts.js
@@ -0,0 +1,24 @@
+var opiniaonavval
+var opiniaocompraval
+var recomendarval
+
+
+
+function opiniaonav(avaliacao){
+    console.log(avaliacao)
+    opiniaoval=avaliacao
+}
+function opiniaocompra(avaliacao){
+    console.log(avaliacao)
+    opiniaocompraval=avaliacao
+}
+function recomendar(avaliacao){
+    console.log(avaliacao)
+    recomendarval=avaliacao
+}
+
+function submeteropiniao(){
+    console.log(opiniaoval)
+    console.log(opiniaocompraval)
+    console.log(recomendarval)
+}
